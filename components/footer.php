8bdaab63967e54ee23c7b86b0969800417290fd4
sanjeep1 <sanjeepbanjara@gmail.com>
2023-01-11 17:07:34 +0545

    foodorder

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our email</h3>
+         <a href="mailto:banjarasanjeep@gmail.com">banjarasanjeep@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening hours</h3>
+         <p>07:00am to 09:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="https://www.google.com/maps/place/Thamel,+Kathmandu+44600/@27.7150086,85.3078937,16z/data=!3m1!4b1!4m5!3m4!1s0x39eb18fcb77fd4bd:0x58099b1deffed8d4!8m2!3d27.7153902!4d85.3123293">Thamel, Kathmandu</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Our number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Sanjeep Banjara</span> | all rights reserved!</div>
+
+</footer>
+
---
+++ update_address.php
@@ -0,0 +1,83 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+   header('location:home.php');
+};
+
+if(isset($_POST['submit'])){
+
+   $address = $_POST['houseno'] .', '.$_POST['area'].', '.$_POST['city'] .', '. $_POST['state'] .', '. $_POST['country'] .' - '. $_POST['pin_code'].','.$_POST['landmark'];
+   $address = filter_var($address, FILTER_SANITIZE_STRING);
+
+   $update_address = $conn->prepare("UPDATE `users` set address = ? WHERE id = ?");
+   $update_address->execute([$address, $user_id]);
+
+   $message[] = 'address saved!';
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>update address</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php' ?>
+
+<section class="form-container">
+
+   <form action="" method="post">
+      <h3>your address</h3>
+      <input type="text" class="box" placeholder="houseno" required maxlength="50" name="houseno">
+      <input type="text" class="box" placeholder="area name" required maxlength="50" name="area">
+      <input type="text" class="box" placeholder="city name" required maxlength="50" name="city">
+      <input type="text" class="box" placeholder="state name" required maxlength="50" name="state">
+      <input type="text" class="box" placeholder="country name" required maxlength="50" name="country">
+       <input type="text" class="box" placeholder="landmark(optional)" required maxlength="50" name="landmark">
+      <input type="number" class="box" placeholder="pin code" required max="999999" min="0" maxlength="6" name="pin_code">
+      <input type="submit" value="save address" name="submit" class="btn">
+   </form>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php' ?>
+
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
